fc5d61438d9aec030c3c342b8ec5c39f996a5788
msalomonsson <69800375+msalomonsson@users.noreply.github.com>
2021-11-26 12:17:33 +0100

    added http hook

---
+++ frontend/src/utils/apiHttp.js
@@ -0,0 +1,39 @@
+import { useCallback, useState } from "react";
+import { useDispatch } from "react-redux";
+
+const useHttp = () => {
+  const [loading, setloading] = useState(false);
+  const [error, seterror] = useState(null);
+  const dispatch = useDispatch();
+
+  const request = useCallback(
+    async (requestObj, dispatchFunc) => {
+      setloading(true);
+      seterror(null);
+
+      try {
+        const response = await fetch(requestObj.url, {
+          method: requestObj.method ? requestObj.method : "GET",
+          headers: requestObj.headers ? requestObj.headers : {},
+          body: requestObj.body ? JSON.stringify(requestObj.body) : null,
+        });
+
+        if (!response.ok) {
+          throw new Error(`request failed ${response.status}`);
+        }
+
+        const data = await response.json();
+        dispatch(dispatchFunc(data));
+      } catch (error) {
+        seterror(error.message || "Something went wrong");
+      }
+
+      setloading(false);
+    },
+    [dispatch]
+  );
+
+  return { loading, error, request };
+};
+
+export default useHttp;
