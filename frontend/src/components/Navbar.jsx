553049d291884e37fc756871794fc45deedbfb47
IbrahimNassman <69851465+IbrahimNassman@users.noreply.github.com>
2021-12-06 22:33:10 +0100

    add navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,18 +6,21 @@ const navbar = () => {
 
   
   return (
-      <nav className="bg-blue-700 sticky text-base">
-        <div className="logo">
-          <Link to="/">
-            <img className="logo" src={logo} width="100rem" alt="Logo" />
-          </Link>
-        </div>
-        <div className="login-btn">
-          <button type="button" className="button">
-            login
-          </button>
-        </div>
-      </nav>
+    <div className="bg-background w-full sticky top-0 z-50 let-0 text-base flex justify-between items-center p-4 shadow-md ">
+      <div className="logo">
+        <Link to="/">
+          <img className="logo" src={logo} width="100rem" alt="Logo" />
+        </Link>
+      </div>
+      <div>
+        <button className="bg-green-700 text-white py-2 px-4 rounded inline-flex items-center">
+          <span>Signin</span>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+          </svg>
+        </button>
+      </div>
+    </div>
   );
 };
 export default navbar;
