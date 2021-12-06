2888810d3124071eba23a1666578f943016ab6e9
IbrahimNassman <69851465+IbrahimNassman@users.noreply.github.com>
2021-12-06 09:36:38 +0100

    add navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,77 +1,23 @@
 import React from "react";
-import { Link, CheckLoggin } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "./assets/logo.svg";
 
 const navbar = () => {
 
   
   return (
-    <>
-      <div>
-        <div>
+      <nav className="bg-blue-700 sticky text-base">
+        <div className="logo">
           <Link to="/">
             <img className="logo" src={logo} width="100rem" alt="Logo" />
           </Link>
         </div>
-        <h1>This is navbar</h1>
-      </div>
-    </>
+        <div className="login-btn">
+          <button type="button" className="button">
+            login
+          </button>
+        </div>
+      </nav>
   );
 };
-
-class CheckLoggin extends React.Component{
-  constructor(props){
-    super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = {isLoggedIn: false};
-  }
-      handleLoginClick() {
-    this.setState({isloggedIn:true});
-  }
-      handleLogoutClick() {
-    this.setState({isloggedIn:false});
-  }
-  
-render(){
-  const isloggedIn = this.state.isloggedIn;
-    let button = null;
-      if (isloggedIn) {
-        button = 
-          <LogoutButton onClick = {this.handleLogoutClick} />;} 
-            else { button =
-          <LoginButton onClick = {this.handleLoginClick} />;}
-          return (
-    <div>
-      <Greeting isloggedIn={isloggedIn} />
-      {button}
-    </div>
-);}}
-function IamLoggedIn(props){
-return <h1>Welcome Back </h1>}
-
-function IamLoggedOut(props){
-  return 
-    <h1>Please Sign Up</h1>}
-
-function Greeting(props){
-  const isloggedIn = props.isloggedIn;
-    if(isloggedIn){
-      return <IamLoggedIn />} 
-        else {
-          return  <IamLoggedOut />}}
-
-function LoginButton(props){
-  return (
-    <button onClick = {props.onClick}>
-      LogIn
-    </button>)}
-
-function LogoutButton(props){
-return (
-  <button onClick = {props.onClick}>
-    LogOut
-    </button>)}
-
-
 export default navbar;
---
+++ server/keys.js
@@ -0,0 +1,5 @@
+exports.GOOGLE_CLIENT_ID ="181993892239-dfik2vep1afl5ad52qvtc9pu3njf3g26.apps.googleusercontent.com";
+
+exports.GOOGLE_CLIENT_SECRET = "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";
+
+exports.SESSION_SECRET = "Forumhemlighet";
